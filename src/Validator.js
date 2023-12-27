0de3e7cc30b68fe0107f07b5bc25461745cf691b
kwaz1k <petenka100@gmail.com>
2023-12-28 01:52:42 +0300

    test

---
+++ src/Validator.js
@@ -0,0 +1,41 @@
+// src/index.js
+
+class Validator {
+  number() {
+    return {
+      isValid: (value) => typeof value === 'number' && !isNaN(value),
+    };
+  }
+
+  array() {
+    return {
+      isValid: (value) => Array.isArray(value),
+      allIntegers: () => ({
+        isValid: (arr) => Array.isArray(arr) && arr.every((element) => typeof element === 'number' && !isNaN(element) && Number.isInteger(element)),
+      }),
+      custom: (callback) => ({
+        isValid: (arr) => Array.isArray(arr) && arr.every(callback),
+      }),
+    };
+  }
+
+  object() {
+    return {
+      shape: (shape) => ({
+        isValid: (obj) => {
+          if (typeof obj !== 'object' || obj === null) {
+            return false;
+          }
+
+          const keys = Object.keys(shape);
+          return keys.every((key) => {
+            const validator = shape[key];
+            return validator && validator.isValid && validator.isValid(obj[key]);
+          });
+        },
+      }),
+    };
+  }
+}
+
+export default Validator;
